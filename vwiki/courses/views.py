94a5311693b3d21b6b4c24f240149d3a97536a46
Victor Arnaud <victorhad@gmail.com>
2017-02-20 02:36:53 -0300

    courses form

--- vwiki/courses/views.py
+++ vwiki/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import TopicForm
 
 
 def course(request, course_slug):
@@ -9,3 +10,13 @@ def course(request, course_slug):
   return render(request, template, context)
 
 
+def create_topic(request, course_slug):
+  template = 'courses/topic_create.html'
+  course = get_object_or_404(Course, slug=course_slug)
+  topic = Topic(course=course)
+  form = TopicForm(request.POST or None, instance=topic)
+  if form.is_valid():
+    form.save()
+    redirect('courses:course', kwargs=(course.id,))
+  context = {'form': form}
+  return render(request, template, context)
